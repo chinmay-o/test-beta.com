8bc5b083a695a9181e5731490787d5533328fceb
chinmay866 <37858624+chinmay866@users.noreply.github.com>
2022-01-06 23:29:44 +0530

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -14,7 +14,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCMjrV4KyG66HRNwiV3q5Z3wqIcWzvaADA",
   authDomain: "dummy-trial-7e07a.firebaseapp.com",
   projectId: "dummy-trial-7e07a",
-  storageBucket: "dummy-trial-7e07a.appspot.com",
+  storageBucket: "dummy-trial-7e07a-default-rtdb",
   messagingSenderId: "1072340910428",
   appId: "1:1072340910428:web:d6277b44d83443e24fc199",
   measurementId: "G-JBRN70ZBXG"
