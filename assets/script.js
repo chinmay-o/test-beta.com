5e5bd4ce9499cd2f20ca5e2db5eb81961f33f42d
chinmay866 <37858624+chinmay866@users.noreply.github.com>
2022-01-06 23:36:27 +0530

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
 // Import the functions you need from the SDKs you need
-import {
-  initializeApp
-} from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-import {
-  getAnalytics
-} from "https://www.gstatic.com/firebasejs/9.6.1/firebase-analytics.js";
+// import {
+//   initializeApp
+// } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+// import {
+//   getAnalytics
+// } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-analytics.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
